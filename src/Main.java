e68d430cfdb1aa2b02b7f5da0f388ad60ab45c91
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-03-15 11:59:01 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
+
+        String word = JOptionPane.showInputDialog("Введите имя: ");
+        if (word != null){
+            JOptionPane.showMessageDialog(null,"Вы ввели имя " + word);
+        }
+
+
+
+
+
 
     }
 }
